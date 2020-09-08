505b6d7ecae5a20493b76ef496cee7a1c56ed34d
monidan <daniil17032002@gmail.com>
2020-09-08 14:54:59 +0300

    Updates

---
+++ precache-manifest.c93b5813c3ed8cf9cd6a066f25e46e1e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7161f4d804f55b0492e0df34aa11e7c4",
+    "url": "/bicycle-site-shop/index.html"
+  },
+  {
+    "revision": "8f6d2304586b7ff04de6",
+    "url": "/bicycle-site-shop/static/css/main.e557d119.chunk.css"
+  },
+  {
+    "revision": "e890a5c426ecd407be37",
+    "url": "/bicycle-site-shop/static/js/2.98a277a2.chunk.js"
+  },
+  {
+    "revision": "9cbaa7e13ab69a72b94de82056a31ae9",
+    "url": "/bicycle-site-shop/static/js/2.98a277a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f6d2304586b7ff04de6",
+    "url": "/bicycle-site-shop/static/js/main.461c9b4c.chunk.js"
+  },
+  {
+    "revision": "0aeb27e340ba400acd46",
+    "url": "/bicycle-site-shop/static/js/runtime-main.307434b0.js"
+  },
+  {
+    "revision": "8b8e9d49e63bbd927f1ef394cc4c976f",
+    "url": "/bicycle-site-shop/static/media/e-bike-article.8b8e9d49.jpg"
+  },
+  {
+    "revision": "62e77166e8a74dc8f360675627865fa7",
+    "url": "/bicycle-site-shop/static/media/header-background-bicycle.62e77166.jpg"
+  }
+]);
